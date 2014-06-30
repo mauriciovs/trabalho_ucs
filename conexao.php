ca34fad6e15c76212ba7e790c9da315ce8287d80
Mauricio Silva <mauriciovs.ti@gmail.com>
2014-06-29 21:41:05 -0300

    Atualização BD

--- conexao.php
+++ conexao.php
@@ -1,4 +1,6 @@
 <?php
-    $conecta = mysql_connect("localhost", "root", "") or print (mysql_error());
+    $conecta = mysql_connect(
+        "https://mauriciovs-trabalho.rhcloud.com", "adminh3RRc9X", "hWWEGglMq5rA")
+        or print (mysql_error());
     mysql_select_db("eleicoes", $conecta) or print(mysql_error());
 ?>
